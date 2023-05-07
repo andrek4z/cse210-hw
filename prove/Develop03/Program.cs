c35f9a2cc82fcc78d9c2c8e7f31dba651804e8a8
andrek4z <andre.fernandez.98@hotmail.com>
2023-05-07 02:00:19 -0500

    Done

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,18 +1,21 @@
 using System;
-
+using System.Collections.Generic;
+using System.IO;
 class Program
 {
     static void Main(string[] args)
     {
+        Reference r = new Reference();
         Console.Write("Please enter the reference of the scripture you want to memorize: ");
         string reference = Console.ReadLine();
+        r.SetReference(reference);
         Console.Write("Please enter the text of the scripture you want to memorize: ");
         string text = Console.ReadLine();
         Console.Clear();
         string answer = "";
         while (answer != "quit")
         {
-            Console.WriteLine($"{reference}: {text}.");
+            Console.WriteLine($"{r.GetReference()}: {text}.");
             Console.WriteLine();
             Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
             answer = Console.ReadLine();
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,25 @@
+public class Scripture
+{
+
+}
+
+public class Reference
+{
+    private string _reference;
+    
+    public string GetReference()
+    {
+        return _reference;
+    }
+
+    public void SetReference(string Reference)
+    {
+        _reference = Reference;
+    }
+
+}
+
+public class Word
+{
+
+}
