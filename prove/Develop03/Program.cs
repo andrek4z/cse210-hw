13b8ab4c9378da26f1de83c24ba634ee0d52dc97
andrek4z <andre.fernandez.98@hotmail.com>
2023-05-06 23:57:08 -0500

    Done

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.Write("Please enter the reference of the scripture you want to memorize: ");
+        string reference = Console.ReadLine();
+        Console.Write("Please enter the text of the scripture you want to memorize: ");
+        string text = Console.ReadLine();
+        Console.Clear();
+        string answer = "";
+        while (answer != "quit")
+        {
+            Console.WriteLine($"{reference}: {text}.");
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
+            answer = Console.ReadLine();
+            Console.Clear();
+        }
     }
 }
